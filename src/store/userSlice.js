4865be3be07659483e48396467020414f4137fe6
dasushan <53913896+dasushan@users.noreply.github.com>
2024-11-26 14:09:17 +0530

    storing email is localStorage

--- src/store/userSlice.js
+++ src/store/userSlice.js
@@ -3,7 +3,14 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = {
     property: [],
     selectedCategory: null,
-    bookingIsOpen: false,
+    
+
+    showBookingModal: false,
+    bookedProperty: null,
+
+    token: null,
+    emailId: '',
+    isLoggedIn: false
 }
 
 const userSlice = createSlice({
@@ -18,9 +25,36 @@ const userSlice = createSlice({
         },
         setBookingIsOpen: (state, action) => {
             state.bookingIsOpen = action.payload
+        },
+
+        setShowBookingModal: (state, action) => {
+            state.showBookingModal = action.payload
+        },
+        setBookedProperty: (state, action) => {
+            state.bookedProperty = action.payload
+        },
+        login: (state, action) => {
+            state.token = action.payload.idToken;
+            state.emailId = action.payload.email;
+            localStorage.setItem('token', state.token)
+            localStorage.setItem('email', state.emailId)
+            state.isLoggedIn = true;
+        },
+        logout: (state, action) => {
+            state.token = null;
+            state.emailId = '';
+            localStorage.removeItem('token');
+            localStorage.removeItem('email');
+            state.isLoggedIn = false;
+        },
+        getAuthStatus: (state, action) => {
+            const token = localStorage.getItem('token');
+            state.emailId = localStorage.getItem('email');
+            state.token = token;
+            state.isLoggedIn = !!token;
         }
     }
 })
 
-export const { setCategory, setProperty, setBookingIsOpen} = userSlice.actions
+export const { setCategory, setProperty, setBookingIsOpen, setShowBookingModal, setBookedProperty, login, logout, getAuthStatus} = userSlice.actions
 export default userSlice
